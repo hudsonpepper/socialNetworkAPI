6308a60eb4d1222068aaf803768ed89d2cf16af1
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2024-01-30 09:15:45 -0500

    made models in accordance with README

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dayjs = require('dayjs');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  },
+);
+
+
+thoughtSchema.virtual('formatDate').get(function () {
+  return dayjs(this.createdAt).format('MM/DD/YYYY')
+});
+
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
